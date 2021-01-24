4fa994b7389478b0cd5962ae4e37301e07210f7a
mariaconejo <mary03081@gmail.com>
2021-01-23 20:38:20 -0600

    Se agrego js

---
+++ script.js
@@ -0,0 +1,43 @@
+/**
+ * Ejercicio #3:
+ * Usando querySelector y querySelectorAll
+ *
+ * ¿Que voy a evaluar?
+ * 1. Que funcione
+ * 2. Buenas practicas
+ *
+ * NOTA: no se puede modificar el codigo HTML de la pagina
+ */
+
+/*
+  EJERCICIO A: --------------------------------------------
+  
+  Selecciona y console.log el <h3> que dice "Esto
+  es una lista"
+*/
+
+/*
+  EJERCICIO B: --------------------------------------------
+  
+  Selecciona y loguea el primer y el ultimo elemento
+  de la lista
+*/
+
+/*
+  EJERCICIO C: --------------------------------------------
+  
+  Selecciona y loguea el input de tipo "email" del
+  formulario
+*/
+
+/*
+  EJERCICIO D: --------------------------------------------
+  
+  Selecciona y loguea la fila de la tabla que tiene "JavaScript"
+*/
+
+/*
+  EJERCICIO E: --------------------------------------------
+  
+  Selecciona y loguea la celda que dice CSS
+*/
